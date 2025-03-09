46a9c1e4be91746c50c6c25005ee625df50727da
eao2 <enkh.amar.001@gmail.com>
2025-03-09 19:16:27 +0800

    up

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,11 @@
 module.exports = { apps: [ 
-    { name: 'ne-to-staff-beta',
+    { name: 'ne-to-staff-eh-beta',
         script: './.output/server/index.mjs',
         exec_mode: 'cluster',
         instances: 'max',
         env: {
-            PORT: 4100,
-            NITRO_PORT: 4100,
+            PORT: 4200,
+            NITRO_PORT: 4200,
             NODE_ENV: 'production',
             DATABASE_URL: "mysql://admin:neto24@localhost:3306/neto"
         }
